d8051186f444738a4f349fa20a6892773b0fddbc
Simon Wagner <axdecces@gmail.com>
2021-06-02 10:40:00 +0200

    Added return function to class

--- Logic.py
+++ Logic.py
@@ -1,7 +1,20 @@
-class Logic():
+class Logic:
     def __init__(self, xcoor, ycoor):
-        coordinates = []
-        for x in range(xcoor):
-            coordinates[x] = []
-            for y in range(ycoor):
-                coordinates[x].append(0)
+        self.coordinates = []
+        self.xcoor = xcoor
+        self.ycoor = ycoor
+        for x in range(self.xcoor):
+            self.coordinates.append([])
+            for y in range(self.ycoor):
+                self.coordinates[x].append(0)
+
+    def __str__(self):
+        returnstring = ''
+        for y in range(self.ycoor):
+            row = ''
+            for x in range(self.xcoor):
+                row += ' {}'.format(self.coordinates[x][y])
+            returnstring += row + '\n'
+        return returnstring
+
+

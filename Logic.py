c66a5275c09dceeccf358f63456641e0da6d2072
AdrianNeulinger <adrian@neulinger.info>
2021-06-02 11:32:16 +0200

    added getNeighbours function

--- Logic.py
+++ Logic.py
@@ -1,28 +1,37 @@
 class Logic:
-    def __init__(self, xcoor, ycoor):
+    def __init__(self, xCoor, yCoor):
         self.coordinates = []
-        self.xcoor = xcoor
-        self.ycoor = ycoor
-        for x in range(self.xcoor):
+        self.xCoor = xCoor
+        self.yCoor = yCoor
+        for x in range(self.xCoor):
             self.coordinates.append([])
-            for y in range(self.ycoor):
+            for y in range(self.yCoor):
                 self.coordinates[x].append(0)
 
     def update(self):
-        for x in range(self.xcoor):
-            for y in range(self.ycoor):
+        for x in range(self.xCoor):
+            for y in range(self.yCoor):
                 pass
                 # function to determine future state of a single cell
 
+    def getNeigbours(self, xCoor, yCoor):
+        aliveNeigbours = 0
+        for xOffset in range (-1, 2):
+            for yOffset in range(-1, 2):
+                if xOffset == 0 and yOffset == 0:
+                    continue
+                aliveNeigbours += self.coordinates[xCoor + xOffset][yCoor + yOffset]
+        return aliveNeigbours
+
 
     def draw(self):
-
+        pass
 
     def __str__(self):
         returnstring = ''
-        for y in range(self.ycoor):
+        for y in range(self.yCoor):
             row = ''
-            for x in range(self.xcoor):
+            for x in range(self.xCoor):
                 row += ' {}'.format(self.coordinates[x][y])
             returnstring += row + '\n'
         return returnstring
--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+from Logic import Logic
 from Game import Game
+from Logic import Logic
 
-game = Game()
+'''
+logic = Logic(20,20)
+logic.coordinates[0][5] = 1
+logic.coordinates[1][4] = 1
+logic.coordinates[2][5] = 1
+logic.coordinates[3][4] = 1
+logic.coordinates[0][8] = 1
+print(logic)
+
+print(logic.getNeigbours(0,5))
+'''
+#game = Game()
